58f5d4252e09cfdf70dcde0d720beecb3adf45eb
SupahNickie <nicholascase@live.com>
2013-11-13 22:19:36 -0800

    Fixed a copy/paste error on my own controller method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,10 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create!(params[:task])
-
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 
   def update
